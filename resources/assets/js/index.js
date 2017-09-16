40e40e692d872a6dca48469a194ebddec8df8670
r-sede <r_sede@orange.fr>
2017-09-16 16:26:09 +0200

    ouech

---
+++ resources/assets/js/index.js
@@ -0,0 +1,8 @@
+document.ready(function() {
+var map = L.map('mapid').setView([43.650000, 0.583333], 9);
+L.tileLayer('http://{s}.tile.osm.org/{z}/{x}/{y}.png', {
+    attribution: '&copy; <a href="http://osm.org/copyright">OpenStreetMap</a> contributors'
+}).addTo(map);
+
+map.zoomControl.setPosition('topright');
+});
---
+++ resources/views/createJardin.blade.php
@@ -0,0 +1,29 @@
+<!DOCTYPE html>
+<html>
+<head>
+  <meta name="viewport" content="width=device-width, initial-scale=1.0 , maximum-scale=1 , user-scalable=no"/>
+  <meta charset="utf-8"/>
+  <meta name="csrf-token" content="{{ csrf_token() }}"/>
+  <title>{{ config('app.name') }}</title>
+  <meta name="Robots" content="all"/>
+  <link rel="stylesheet" href="css/leaflet.css">
+</head>
+<body>
+
+
+
+  
+    <div id="mapid"></div>
+    
+
+
+
+
+
+
+    <script type="text/javascript" src="js/jquery/dist/jquery.min.js" ></script>
+    <script type="text/javascript" src="js/leaflet.js"></script>
+
+      <script type="text/javascript" src="js/index.js"></script>
+  </body>
+  </html>

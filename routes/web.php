39f53bfcddbc9c21eb0ab7c4b9fa89c79167fb2a
r-sede <r_sede@orange.fr>
2017-09-16 16:56:35 +0200

    all

--- routes/web.php
+++ routes/web.php
@@ -15,6 +15,9 @@ Route::get('/', function () {
     return view('acceuil');
 });
 
+Route::get('/test', function () {
+    return view('fullMap');
+});
 Auth::routes();
 
 Route::get('/home', 'HomeController@index')->name('home');

b87f57af8ba031d46c17e7a12a277e3f57e2d7cb
MorelCisse <MorelCisse19@gmail.com>
2017-09-16 11:51:59 +0200

    Initial Commit

---
+++ resources/views/acceuil.blade.php
@@ -0,0 +1,22 @@
+@extends("layouts.app2")
+
+@section('content')
+<div class="container">
+	<div class="row">
+		<div class="col">
+			<div class="card text-white bg-success mb-3" style="width: 20rem; height: 17rem">
+				<a href="#" class="card-body">
+					<h4 class="card-title">J'ai un jardin</h4>
+				</a>
+			</div>
+		</div>
+		<div class="col">
+			<div class="card text-white bg-success mb-3" style="width: 20rem; height: 17rem">
+				<a href="#" class="card-body">
+					<h4 class="card-title">Je veux jardiner</h4>
+				</a>
+			</div>
+		</div>
+	</div>
+</div>
+@endsection	
---
+++ resources/views/layouts/app2.blade.php
@@ -0,0 +1,26 @@
+<!DOCTYPE html>
+<html lang="fr">
+<head>
+	<meta charset="utf-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+
+	<title>Adopte-ma-tomate</title>
+	<link href="https://fonts.googleapis.com/css?family=Cabin" rel="stylesheet">
+	<link rel="stylesheet" type="text/css" href="css/bootstrap.css">
+	<link rel="stylesheet" type="text/css" href="css/acceuil.css">
+</head>
+<body>
+
+	<!-- Image and text -->
+	<nav class="navbar navbar-light">
+		<a class="navbar-brand" href="#">
+			<img src="" width="30" height="30" class="d-inline-block align-top" alt="">
+			ADOPTE-MA-TOMATE
+		</a>
+	</nav>
+
+	@yield('content')
+
+	<script type="text/javascript" src="js/bootstrap.js"></script>
+</body>
+</html>
